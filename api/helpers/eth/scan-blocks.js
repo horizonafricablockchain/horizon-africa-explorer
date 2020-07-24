10c1316defd4d8e8ed6f471969664bd0eb3bc292
Suyash Sumaroo <suyash515@gmail.com>
2020-07-24 09:40:07 +0400

    added async way of retrieving block information

--- api/helpers/eth/scan-blocks.js
+++ api/helpers/eth/scan-blocks.js
@@ -19,56 +19,73 @@ module.exports = {
 
     fn: async function(inputs, exits) {
         var blockNumberArray = [],
-            blockArray = [],
-            transactionArray = [],
-            blockDetails,
+            // blockArray = [],
+            // transactionArray = [],
+            // blockDetails,
             tokens = await Token.find(),
-            blockRecord = {};
+            // blockRecord = {},
+            blockNumber = 0;
 
         for (var i = inputs.start; i <= inputs.end; i++) {
             blockNumberArray.push(i);
         }
 
         for (var i = 0; i < blockNumberArray.length; i++) {
-            blockDetails = await sails.helpers.eth.web3.getBlock(blockNumberArray[i], true);
+            blockNumber = blockNumberArray[i];
 
-            if (blockDetails && blockDetails.data) {
-                if (blockDetails.data.transactions && blockDetails.data.transactions.length > 0) {
-                    _.each(blockDetails.data.transactions, function(tx) {
-                        transactionArray.push(tx);
+            (function(block) {
+                var transactionArray = [],
+                    blockRecord = {},
+                    blockArray = [];
+
+                sails.helpers.eth.web3.getBlock(block, true)
+                    .then(function(blockDetails) {
+                        if (blockDetails && blockDetails.data) {
+                            if (blockDetails.data.transactions && blockDetails.data.transactions.length > 0) {
+                                _.each(blockDetails.data.transactions, function(tx) {
+                                    transactionArray.push(tx);
+                                });
+                            }
+
+                            blockRecord = {
+                                author: blockDetails.data.author,
+                                difficulty: blockDetails.data.difficulty,
+                                extraData: blockDetails.data.extraData,
+                                gasLimit: blockDetails.data.gasLimit,
+                                gasUsed: blockDetails.data.gasUsed,
+                                hash: blockDetails.data.hash,
+                                hash_lower: blockDetails.data.hash.toLowerCase(),
+                                miner: blockDetails.data.miner,
+                                number: blockDetails.data.number,
+                                parentHash: blockDetails.data.parentHash,
+                                parentHash_lower: blockDetails.data.parentHash.toLowerCase(),
+                                receiptsRoot: blockDetails.data.receiptsRoot,
+                                sha3Uncles: blockDetails.data.sha3Uncles,
+                                size: blockDetails.data.size,
+                                stateRoot: blockDetails.data.stateRoot,
+                                timestamp: blockDetails.data.timestamp,
+                                totalDifficulty: blockDetails.data.totalDifficulty,
+                                transactionsRoot: blockDetails.data.transactionsRoot
+                            }
+
+                            console.log("created block element for: " + blockDetails.data.number);//debug
+
+                            return EthBlock.create(blockRecord).fetch();
+                        }
+                    }).then(function(createdBlock) {
+                        return sails.helpers.eth.extractTransactions([createdBlock], transactionArray, tokens);
+                    }).then(function() {
+                        console.log("completed processing block " + block);//debug
                     });
-                }
-
-                blockRecord = {
-                    author: blockDetails.data.author,
-                    difficulty: blockDetails.data.difficulty,
-                    extraData: blockDetails.data.extraData,
-                    gasLimit: blockDetails.data.gasLimit,
-                    gasUsed: blockDetails.data.gasUsed,
-                    hash: blockDetails.data.hash,
-                    hash_lower: blockDetails.data.hash.toLowerCase(),
-                    miner: blockDetails.data.miner,
-                    number: blockDetails.data.number,
-                    parentHash: blockDetails.data.parentHash,
-                    parentHash_lower: blockDetails.data.parentHash.toLowerCase(),
-                    receiptsRoot: blockDetails.data.receiptsRoot,
-                    sha3Uncles: blockDetails.data.sha3Uncles,
-                    size: blockDetails.data.size,
-                    stateRoot: blockDetails.data.stateRoot,
-                    timestamp: blockDetails.data.timestamp,
-                    totalDifficulty: blockDetails.data.totalDifficulty,
-                    transactionsRoot: blockDetails.data.transactionsRoot
-                }
-
-                blockArray.push(blockRecord);
-            }
+            })(blockNumber);
         }
 
-        var blocksCreated = await EthBlock.createEach(blockArray)
-            .fetch();
+        // var blocksCreated = await EthBlock.createEach(blockArray)
+        //     .fetch();
 
-        await sails.helpers.eth.extractTransactions(blocksCreated, transactionArray, tokens);
+        // await sails.helpers.eth.extractTransactions(blocksCreated, transactionArray, tokens);
 
-        return exits.success(blocksCreated);
+        return exits.success({});
+        // return exits.success(blocksCreated);
     }
 };
