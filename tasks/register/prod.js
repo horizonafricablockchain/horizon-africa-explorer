0a64b3573a7f34a858f62f6ab39fd31060e03f87
Suyash Sumaroo <suyash515@gmail.com>
2021-05-05 22:46:44 +0400

    changed prod settings

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -13,11 +13,11 @@
  */
 module.exports = function(grunt) {
   grunt.registerTask('prod', [
-    'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
+    // 'polyfill:prod', //« Remove this to skip transpilation in production (not recommended)
     'compileAssets',
-    'babel',         //« Remove this to skip transpilation in production (not recommended)
-    'concat',
-    'uglify',
+    // 'babel',         //« Remove this to skip transpilation in production (not recommended)
+    // 'concat',
+    // 'uglify',
     'cssmin',
     'sails-linker:prodJs',
     'sails-linker:prodStyles',
