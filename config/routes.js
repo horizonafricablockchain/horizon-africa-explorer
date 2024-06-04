0715b5340cbcae23a68abf4810ebd9f9ff5fdba1
Elliot <elliot@codeaura.ai>
2024-06-04 19:39:32 -0400

    Create 'delete-block.js' controller for deleting blocks by block number.

--- config/routes.js
+++ config/routes.js
@@ -1,55 +1,46 @@
-module.exports.routes = {
-    "get /": {
-        action: "views/home"
+module.exports = {
+    friendlyName: "Delete block",
+    description: "Delete block by block number",
+    inputs: {
+        block_number: {
+            type: "number",
+            required: true
+        }
     },
-
-    "get /block/:block_number": {
-        action: "views/block/view-block"
+    exits: {
+        success: {
+            responseType: "jsonOk"
+        },
+        notFound: {
+            responseType: "jsonError",
+            statusCode: 404
+        },
+        invalid: {
+            responseType: "jsonError",
+            statusCode: 400
+        }
     },
-
-    "get /block/transaction/:block_number": {
-        action: "views/transaction/view-block-transactions"
-    },
-
-    "get /transaction/:transaction_hash": {
-        action: "views/transaction/view-transaction"
-    },
-
-    "get /address/:address": {
-        action: "views/address/view-address"
-    },
-
-    "get /token/list": {
-        action: "views/token/view-token-list"
-    },
-
-    "get /token/transaction/:address": {
-        action: "views/token/view-token-transaction"
-    },
-
-
-    /* CRONS */
-    "post /cron/scan-blocks": {
-        action: "cron/scan-blocks"
-    },
-
-    "post /cron/extract-token-transaction": {
-        action: "cron/extract-token-transaction"
-    },
-
-
-    /* API */
-    /* TRANSACTIONS API */
-    "post /api/v1/transaction/list": {
-        action: "transaction/list-transaction"
-    },
-
-    "post /api/v1/token-transaction/list": {
-        action: "token-transaction/list-token-transaction"
-    },
-
-    /* TOKEN API */
-    "post /api/v1/token/add": {
-        action: "token/add-token"
+    fn: async function (inputs, exits) {
+        try {
+            const block = await EthBlock.findOne({ number: inputs.block_number });
+            
+            if (!block) {
+                return exits.notFound({
+                    message: `Block number ${inputs.block_number} not found.`
+                });
+            }
+            
+            await EthTransaction.destroy({ blockNumber: inputs.block_number });
+            await EthBlock.destroy({ id: block.id });
+            
+            return exits.success({
+                message: `Block number ${inputs.block_number} and its transactions have been deleted.`
+            });
+        } catch (error) {
+            return exits.invalid({
+                message: 'An error occurred during deletion',
+                error
+            });
+        }
     }
 };
