6557a0ea584894b64935de122927b716fea2abda
Elliot <elliot@codeaura.ai>
2024-06-07 11:00:50 -0400

    Add try/catch blocks to 'get-block-number.js' for better error handling

--- api/helpers/eth/web3/get-block-number.js
+++ api/helpers/eth/web3/get-block-number.js
@@ -5,9 +5,13 @@ module.exports = {
     inputs: {},
 
     fn: async function(inputs, exits) {
-        var node = await sails.helpers.node.getReadNode();
-        var blockNumber = await sails.helpers.utilities.apiUtility("/block/get-block-number", "POST", {});
-
-        return exits.success(blockNumber);
+        try {
+            var node = await sails.helpers.node.getReadNode();
+            var blockNumber = await sails.helpers.utilities.apiUtility("/block/get-block-number", "POST", {});
+            return exits.success(blockNumber);
+        } catch (error) {
+            sails.log.error("Error in get-block-number.js: ", error);
+            return exits.error(error);
+        }
     }
 };
