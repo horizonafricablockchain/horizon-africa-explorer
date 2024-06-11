36cc946def4b8ec097f43a83c2cc691fb8b22b88
Elliot <elliot@codeaura.ai>
2024-06-11 08:54:31 -0600

    Add route for delete block endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,55 +1,7 @@
 module.exports.routes = {
-    "get /": {
-        action: "views/home"
-    },
+  // Other routes...
 
-    "get /block/:block_number": {
-        action: "views/block/view-block"
-    },
+  'DELETE /block/:block_number': { action: 'block/delete-block' }
 
-    "get /block/transaction/:block_number": {
-        action: "views/transaction/view-block-transactions"
-    },
-
-    "get /transaction/:transaction_hash": {
-        action: "views/transaction/view-transaction"
-    },
-
-    "get /address/:address": {
-        action: "views/address/view-address"
-    },
-
-    "get /token/list": {
-        action: "views/token/view-token-list"
-    },
-
-    "get /token/transaction/:address": {
-        action: "views/token/view-token-transaction"
-    },
-
-
-    /* CRONS */
-    "post /cron/scan-blocks": {
-        action: "cron/scan-blocks"
-    },
-
-    "post /cron/extract-token-transaction": {
-        action: "cron/extract-token-transaction"
-    },
-
-
-    /* API */
-    /* TRANSACTIONS API */
-    "post /api/v1/transaction/list": {
-        action: "transaction/list-transaction"
-    },
-
-    "post /api/v1/token-transaction/list": {
-        action: "token-transaction/list-token-transaction"
-    },
-
-    /* TOKEN API */
-    "post /api/v1/token/add": {
-        action: "token/add-token"
-    }
+  // Other routes...
 };
