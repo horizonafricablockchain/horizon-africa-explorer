2c0a99c078bbe3ea6e9c1c584e415535027d4e4b
Elliot <elliot@codeaura.ai>
2024-08-30 04:01:22 -0400

    Replicated get-read-node.js functionality in PHP.

---
+++ api/helpers/node/php/get-write-node.php
@@ -0,0 +1,18 @@
+<?php
+class GetWriteNode {
+    public static function execute() {
+        // Assuming EthereumNode is a model class that you have defined elsewhere
+        $nodeArray = EthereumNode::find(['status' => EthereumNode::STATUS_ACTIVE]);
+
+        $nodeToReturn = null;
+        if (count($nodeArray) > 0) {
+            $nodeToReturn = $nodeArray[0];
+        }
+
+        return $nodeToReturn;
+    }
+}
+
+// Usage example
+$result = GetWriteNode::execute();
+?>
