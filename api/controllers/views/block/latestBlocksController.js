8d45ce77f5b6becaf9739c654db8c22ccbfa093e
Elliot <elliot@codeaura.ai>
2024-10-02 12:19:52 -0400

    Create latest-blocks.pug to display the latest 15 blocks.

---
+++ api/controllers/views/block/latestBlocksController.js
@@ -0,0 +1,25 @@
+// latestBlocksController.js
+
+const EthBlock = require('../../../models/EthBlock');
+
+module.exports = {
+    friendlyName: 'View latest blocks',
+
+    description: 'Displays the latest 15 Ethereum blocks.',
+
+    exits: {
+        success: {
+            viewTemplatePath: 'pages/block/latest-blocks'
+        }
+    },
+
+    fn: async function () {
+        // Retrieve the latest 15 blocks ordered by createdAt date
+        const latestBlocks = await EthBlock.find().sort('createdAt DESC').limit(15);
+
+        // Send the blocks to the view
+        return {
+            blocks: latestBlocks
+        };
+    }
+};
