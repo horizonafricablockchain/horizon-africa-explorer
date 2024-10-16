300a52194f089f06fe6a0cda07aeea7b6b11dfe9
Elliot <elliot@codeaura.ai>
2024-10-15 23:49:21 -0400

    Add comments to home.js function to enhance clarity and understanding. Add detailed section comments to home.pug for clarity and maintainability.

--- api/controllers/views/home.js
+++ api/controllers/views/home.js
@@ -1,4 +1,7 @@
 module.exports = {
+    // This function is responsible for displaying the home page of the Ethereum Blockchain Explorer.
+    // It aggregates and prepares blockchain-related data, including Ethereum block tracker,
+    // blocks, and transactions, before sending this data to the view template.
     friendlyName: "Displays home page",
     description: "Displays home page",
     exits: {
@@ -8,12 +11,16 @@ module.exports = {
         }
     },
     fn: async function(inputs, exits) {
+        // Fetch the Ethereum block tracker
         var blockTrackerList = await BlockTracker.find({
                 type: BlockTracker.constants.type.eth
             }),
             blockTracker = null,
+            // Count the total Ethereum transactions
             transactionCount = await EthTransaction.count({}),
-            latestBlockList = await EthBlock.find({}).sort("number DESC").limit(10),
+            // Retrieve the latest 10 blocks sorted by the createdAt date
+            latestBlockList = await EthBlock.find({}).sort("createdAt DESC").limit(10),
+            // Retrieve the latest 10 transactions and populate their associated blocks
             latestTransactionList = await EthTransaction.find({}).populate("eth_block").sort("createdAt DESC").limit(10),
             validatorList = await Validator.find(),
             findValidator = null,
@@ -23,6 +30,7 @@ module.exports = {
             blockTracker = blockTrackerList[0];
         }
 
+        // For each block, count the number of transactions and find the validator
         if (latestBlockList && latestBlockList.length > 0) {
             for (var i = 0; i < latestBlockList.length; i++) {
                 latestBlockList[i].number_transactions = await EthTransaction.count({
@@ -39,25 +47,12 @@ module.exports = {
             }
         }
 
+        // Convert the transaction value from Wei to Ether
         for (var i = 0; i < latestTransactionList.length;i++) {
             latestTransactionList[i].ether_value = web3.utils.fromWei(latestTransactionList[i].value, "ether");
         }
 
-        // for(var i=0;i<latestTransactionList.length;i++) {
-        //     sails.log.debug("home.js (Line: 42) : latestTransactionList[i].input");//debug
-        //     sails.log.debug(latestTransactionList[i].input);//debug
-
-        //     var decoded = web3.utils.toAscii(latestTransactionList[i].input);
-        //     latestTransactionList[i].input
-
-        //     sails.log.debug("home.js (Line: 48) : decoded");//debug
-        //     // sails.log.debug(web3.utils.toAscii(latestTransactionList[i].input));//debug
-        //     // sails.log.debug(web3.utils.hexToAscii(latestTransactionList[i].input));//debug
-        //     // sails.log.debug(web3.utils.hexToString(latestTransactionList[i].input));//debug
-
-        //     break;
-        // }
-
+        // Return the aggregated data to the view
         return exits.success({
             block_tracker: blockTracker,
             transaction_count: transactionCount,
@@ -65,4 +60,4 @@ module.exports = {
             latest_transaction_list: latestTransactionList
         });
     }
-}
+};
